bda515963103d591678d64f02d23442534a54232
Romi Kusuma Bakti <romikusumab@gmail.com>
2023-05-28 22:50:18 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+import Tombol from "./components/Tombol";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <h1>Hello, universe!</h1>
+      <Tombol tulisan="Buat Akun" />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import Tombol from "./Tombol";
+
+function Header() {
+  return (
+    <header>
+      <div>Andromeda</div>
+      <Tombol>Home</Tombol>
+      <Tombol>Login</Tombol>
+    </header>
+  );
+}
+
+export default Header;
